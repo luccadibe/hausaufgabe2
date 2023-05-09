c7a3e215ae255426c257136152951951a8e3f1c9
Lucca Di Benedetto <luccadibe@gmail.com>
2023-05-09 12:44:19 +0200

    Nodejs erste basis

---
+++ hausaufgabe2/server/server.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const dotenv = require("dotenv").config();
+const app = express();
+const port = process.env.PORT;
+
+app.use("/", require("./routes/contactRoutes"));
+app.listen(port, () => {
+  console.log(`Server  running on port ${port}`);
+});
