24cd86638396ecc8a632e2e2c780ae2ce48f41a2
Jakub Kwinta <jak.kwinta@gmail.com>
2023-05-14 13:17:36 +0200

    edittodo v1

--- hausaufgabe2/client/src/App.jsx
+++ hausaufgabe2/client/src/App.jsx
@@ -2,8 +2,9 @@ import NavBar from "./components/NavBar";
 import TodoTable from "./components/TodoTable";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
-import { Route, Routes } from "react-router-dom";
 import New from "./pages/New";
+import EditTodo from "./pages/EditTodo";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/new" element={<New />} />
+        <Route path="/edittodo" element={<EditTodo />} />
       </Routes>
       <Footer />
     </div>
