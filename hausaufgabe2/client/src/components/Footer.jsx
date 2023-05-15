a5d14afca70d337c862c490663815a2c4b160949
Lucca Di Benedetto <luccadibe@gmail.com>
2023-05-15 23:50:06 +0200

    funktionalität new_todo erreicht. API korrektur viele sachen

--- hausaufgabe2/client/src/components/Footer.jsx
+++ hausaufgabe2/client/src/components/Footer.jsx
@@ -2,12 +2,37 @@ import "../App.css";
 
 function Footer() {
   return (
-    <div className="fixed-bottom">
-      <main className="container mt-1">
-        <p>Lucca Di Benedetto , Jakub Kwinta </p>
-        <p> ©TU-Berlin</p>
-      </main>
-    </div>
+    <>
+      <div
+        className="position-fixed bottom-0 end-0 m-3"
+        style={{ zIndex: 9999 }}
+      >
+        <button
+          className="btn btn-primary"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#collapseWidthExample"
+          aria-expanded="false"
+          aria-controls="collapseWidthExample"
+        >
+          Impressum
+        </button>
+      </div>
+
+      <div style={{ minHeight: "120px" }}>
+        <div
+          className="collapse collapse-horizontal position-absolute bottom-0 end-0"
+          id="collapseWidthExample"
+        >
+          <div
+            className="card card-body"
+            style={{ width: "300px", zIndex: 9998 }}
+          >
+            Lucca Di Benedetto ©TU-Berlin
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 export default Footer;
