48643836ae4f18f47d89a34eecd4080c7a498e10
Jakub Kwinta <jak.kwinta@gmail.com>
2023-05-17 02:33:58 +0200

    edittodo works including return to homepage
    i still cant get the actual name , deadline and fortschritt while updating data

--- hausaufgabe2/client/src/pages/EditTodo.jsx
+++ hausaufgabe2/client/src/pages/EditTodo.jsx
@@ -1,9 +1,8 @@
 import React, { useState, useEffect } from "react";
-import { useNavigate, useParams, Link } from "react-router-dom";
+import { useNavigate, useParams, Link, useLocation } from "react-router-dom";
 import axios from "axios";
-import { toast } from "react-toastify";
 
-const initialState = {
+const initialTodos = {
   name: "",
   deadline: "",
   fortschritt: "",
@@ -12,13 +11,12 @@ const initialState = {
 //of the todo !
 //understand - i will try to get it now
 function EditTodo() {
-  const [state, setState] = useState(initialState);
+  const [todo, setTodo] = useState(initialTodos);
 
-  const { name, deadline, fortschritt } = state;
+  const { name, deadline, fortschritt } = todo;
 
-  const history = useNavigate();
+  const navigate = useNavigate();
 
-  ////
   const { id } = useParams();
 
   useEffect(() => {
@@ -26,122 +24,97 @@ function EditTodo() {
       .get(`http://localhost:9000/gettodo/${id}`)
       .then((resp) => setState({ ...resp.data[0] }));
   }, [id]);
-  /////
 
-  const handleSubmit = (e) => {
+  //sendung der neuen Daten an backend server
+  function handleSubmit(e) {
     e.preventDefault();
-    if (!name || !deadline || !fortschritt) {
-      toast.error("Füllen Sie Bitte alle leere Kästchen");
-    } else {
-      // wait but how is this case ever reached? this is the EditTodo code.
-      // ofc the id exists!
-      if (!id) {
-        axios
-          .post("http://localhost:9000/edittodo/", {
-            name,
-            deadline,
-            fortschritt,
-          })
-          .then(() => {
-            setState({ name: "", deadline: "", fortschritt: "" });
-          })
-          .catch((err) => toast.error(err.response.data));
-        toast.success("Todo hinzugefügt");
-      } else {
-        axios
-          .put(`http://localhost:9000/edittodo`, {
-            name: name,
-            deadline: deadline,
-            fortschritt: fortschritt,
-            id: id,
-          })
-          .then(() => {
-            setState({ name: "", deadline: "", fortschritt: "" });
-          })
-          .catch((err) => toast.error(err.response.data));
-        toast.success("Todo bearbeitet");
-      }
-      setTimeout(() => history.push("/"), 400);
+    try {
+      axios
+        .put(`http://localhost:9000/edittodo`, {
+          name,
+          deadline,
+          fortschritt,
+          id: id,
+        })
+        .then(() => {
+          navigate("/");
+        });
+    } catch (err) {
+      console.log(err);
     }
-  };
+  }
 
+  // eingabe der neuen daten
   const handleInputChange = (e) => {
     const { name, value } = e.target;
-    setState({ ...state, [name]: value });
+    setTodo({ ...todo, [name]: value });
   };
 
   return (
     <>
       {" "}
-      <form onSubmit={handleSubmit} />
-      <main className="container mt-4">
-        <h1>Bearbeite TODO</h1>
-      </main>
-      <div className="container mt-3">
-        <div className="input-group mt-3">
-          <span className="input-group-text">
-            <label htmlFor="name">Name</label>
-          </span>
-          <input
-            type="text"
-            id="name"
-            name="name"
-            className="form-control"
-            placeholder="neue Name der Todo eingeben"
-            value={name || ""}
-            onChange={handleInputChange}
-          />
+      <form onSubmit={handleSubmit}>
+        <main className="container mt-4">
+          <h1>Bearbeite TODO</h1>
+        </main>
+        <div className="container mt-3">
+          <div className="input-group mt-3">
+            <span className="input-group-text">
+              <label htmlFor="name">Name</label>
+            </span>
+            <input
+              name="name"
+              className="form-control"
+              placeholder="neue Name der Todo eingeben"
+              value={name}
+              onChange={handleInputChange}
+            />
+          </div>
+
+          <div className="input-group mt-3">
+            <span className="input-group-text">
+              <label htmlFor="deadline">Deadline</label>
+            </span>
+            <input
+              name="deadline"
+              className="form-control"
+              placeholder="neue Datum eingeben"
+              value={deadline}
+              onChange={handleInputChange}
+            />
+          </div>
+
+          <div className="input-group mt-3">
+            <span className="input-group-text">
+              <label htmlFor="fortschritt">Fortschritt</label>
+            </span>
+            <input
+              name="fortschritt"
+              className="form-control"
+              placeholder="neue Prozentwert eingeben"
+              value={fortschritt || ""}
+              onChange={handleInputChange}
+            />
+          </div>
+
+          <div className="d-grid gap-2 mt-3">
+            <button
+              className="btn btn-primary"
+              type="submit"
+              onClick={handleSubmit}
+              to="/"
+            >
+              Speichern
+            </button>
+          </div>
+
+          <div className="d-grid gap-2 mt-3">
+            <Link className="btn btn-secondary" type="button" to="/">
+              Abbrechen
+            </Link>
+          </div>
         </div>
-
-        <div className="input-group mt-3">
-          <span className="input-group-text">
-            <label htmlFor="deadline">Deadline</label>
-          </span>
-
-          <input
-            type="text"
-            id="deadline"
-            name="deadline"
-            className="form-control"
-            placeholder="neue Datum eingeben"
-            value={deadline || ""}
-            onChange={handleInputChange}
-          />
-        </div>
-
-        <div className="input-group mt-3">
-          <span className="input-group-text">
-            <label htmlFor="fortschritt">Fortschritt</label>
-          </span>
-
-          <input
-            type="text"
-            id="fortschritt"
-            name="fortschritt"
-            className="form-control"
-            placeholder="neue Prozentwert eingeben"
-            value={fortschritt || ""}
-            onChange={handleInputChange}
-          />
-        </div>
-
-        <div className="d-grid gap-2 mt-3">
-          <button
-            className="btn btn-primary"
-            type="submit"
-            onClick={handleSubmit}
-            to="/"
-          >
-            Speichern
-          </button>
-        </div>
-
-        <div className="d-grid gap-2 mt-3">
-          <Link className="btn btn-secondary" type="button" to="/">
-            Abbrechen
-          </Link>
-        </div>
-      </div>
+      </form>
     </>
   );
 }
